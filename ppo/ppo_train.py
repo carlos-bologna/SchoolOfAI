49b34cca3fc4bc76cfb2b920c3d4028e63324a27
Carlos Bologna <carlos.bologna@hotmail.com>
2020-05-08 20:08:40 -0300

    Adding more env into code

--- ppo/ppo_train.py
+++ ppo/ppo_train.py
@@ -18,50 +18,63 @@ from torch.distributions import Normal
 from tensorboardX import SummaryWriter
 
 from lib.common import mkdir
-from lib.model import ActorCritic
+#from lib.model import ActorCritic
+import lib.model as models
 from lib.multiprocessing_env import SubprocVecEnv
+
 
 def setup(env_id):
 
-    with open(os.path.join('conf', env_id + '.json'), 'r+') as json_file: #open read and overwrite.
-        
+    # open read and overwrite.
+    with open(os.path.join('conf', env_id + '.json'), 'r+') as json_file:
+
         data = json.load(json_file)
 
         global ENV_ID
-        global NUM_ENVS            
-        global HIDDEN_SIZE         
-        global LEARNING_RATE       
-        global GAMMA               
-        global GAE_LAMBDA          
-        global PPO_EPSILON         
-        global CRITIC_DISCOUNT     
-        global ENTROPY_BETA        
-        global PPO_STEPS           
-        global MINI_BATCH_SIZE     
-        global PPO_EPOCHS          
-        global TEST_EPOCHS         
-        global NUM_TESTS           
-        global TARGET_REWARD       
+        global NUM_INPUTS
+        global NUM_OUTPUTS
+        global NUM_ENVS
+        global HIDDEN_SIZE
+        global LEARNING_RATE
+        global GAMMA
+        global GAE_LAMBDA
+        global PPO_EPSILON
+        global CRITIC_DISCOUNT
+        global ENTROPY_BETA
+        global PPO_STEPS
+        global MINI_BATCH_SIZE
+        global PPO_EPOCHS
+        global TEST_EPOCHS
+        global NUM_TESTS
+        global TARGET_REWARD
+        global MODEL_NAME
 
-        ENV_ID              = data.setdefault('env_id', 'RoboschoolHalfCheetah-v1')
-        NUM_ENVS            = data.setdefault('num_envs', 1)
-        HIDDEN_SIZE         = data.setdefault('hidden_size', 256)
-        LEARNING_RATE       = data.setdefault('learning_rate', 1e-4)
-        GAMMA               = data.setdefault('gamma', 0.99)
-        GAE_LAMBDA          = data.setdefault('gae_lambda', 0.95)
-        PPO_EPSILON         = data.setdefault('ppo_epsilon', 0.2)
-        CRITIC_DISCOUNT     = data.setdefault('critic_discount', 0.5)
-        ENTROPY_BETA        = data.setdefault('entropy_beta', 0.00)
-        PPO_STEPS           = data.setdefault('ppo_steps', 256)
-        MINI_BATCH_SIZE     = data.setdefault('mini_batch_size', 64)
-        PPO_EPOCHS          = data.setdefault('ppo_epochs', 10)
-        TEST_EPOCHS         = data.setdefault('test_epochs', 10)
-        NUM_TESTS           = data.setdefault('num_tests', 10)
-        TARGET_REWARD       = data.setdefault('target_reward', 2500)
+        ENV_ID          = data.setdefault('env_id', 'RoboschoolHalfCheetah-v1')
+        NUM_INPUTS      = data.setdefault('num_inputs', 26)
+        NUM_OUTPUTS     = data.setdefault('num_outputs', 6)
+        NUM_ENVS        = data.setdefault('num_envs', 1)
+        HIDDEN_SIZE     = data.setdefault('hidden_size', 256)
+        LEARNING_RATE   = data.setdefault('learning_rate', 1e-4)
+        GAMMA           = data.setdefault('gamma', 0.99)
+        GAE_LAMBDA      = data.setdefault('gae_lambda', 0.95)
+        PPO_EPSILON     = data.setdefault('ppo_epsilon', 0.2)
+        CRITIC_DISCOUNT = data.setdefault('critic_discount', 0.5)
+        ENTROPY_BETA    = data.setdefault('entropy_beta', 0.00)
+        PPO_STEPS       = data.setdefault('ppo_steps', 256)
+        MINI_BATCH_SIZE = data.setdefault('mini_batch_size', 64)
+        PPO_EPOCHS      = data.setdefault('ppo_epochs', 10)
+        TEST_EPOCHS     = data.setdefault('test_epochs', 10)
+        NUM_TESTS       = data.setdefault('num_tests', 10)
+        TARGET_REWARD   = data.setdefault('target_reward', 2500)
+        MODEL_NAME      = data.setdefault('model_name', 'ActorCritic')
 
-        json_file.seek(0) #go to beggining of file
-        json.dump(data, json_file) #write content
-        json_file.truncate() #clear any tail of old content
+        # Transformations
+        if isinstance(NUM_INPUTS, list): NUM_INPUTS = tuple(NUM_INPUTS)
+
+        json_file.seek(0)  # go to beggining of file
+        json.dump(data, json_file)  # write content
+        json_file.truncate()  # clear any tail of old content
+
 
 def make_env(env_id):
     # returns a function which creates a single environment
@@ -70,7 +83,7 @@ def make_env(env_id):
         return env
     return _thunk
 
-    
+
 def test_env(env, model, device, deterministic=True):
     state = env.reset()
     done = False
@@ -92,13 +105,14 @@ def normalize(x):
     return x
 
 
-def compute_gae(next_value, rewards, masks, values):
+def compute_gae(next_value, rewards, masks, values, gamma=0.99, lam=0.95):
     values = values + [next_value]
     gae = 0
     returns = []
     for step in reversed(range(len(rewards))):
-        delta = rewards[step] + GAMMA * values[step + 1] * masks[step] - values[step]
-        gae = delta + GAMMA * GAE_LAMBDA * masks[step] * gae
+        delta = rewards[step] + gamma * \
+            values[step + 1] * masks[step] - values[step]
+        gae = delta + gamma * lam * masks[step] * gae
         # prepend to get correct order back
         returns.insert(0, gae + values[step])
     return returns
@@ -110,9 +124,9 @@ def ppo_iter(states, actions, log_probs, returns, advantage):
     for _ in range(batch_size // MINI_BATCH_SIZE):
         rand_ids = np.random.randint(0, batch_size, MINI_BATCH_SIZE)
         yield states[rand_ids, :], actions[rand_ids, :], log_probs[rand_ids, :], returns[rand_ids, :], advantage[rand_ids, :]
-        
 
-def ppo_update(frame_idx, states, actions, log_probs, returns, advantages, clip_param=PPO_EPSILON):
+
+def ppo_update(frame_idx, states, actions, log_probs, returns, advantages, clip_param=0.2):
     count_steps = 0
     sum_returns = 0.0
     sum_advantage = 0.0
@@ -131,9 +145,10 @@ def ppo_update(frame_idx, states, actions, log_probs, returns, advantages, clip_
 
             ratio = (new_log_probs - old_log_probs).exp()
             surr1 = ratio * advantage
-            surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
+            surr2 = torch.clamp(ratio, 1.0 - clip_param,
+                                1.0 + clip_param) * advantage
 
-            actor_loss  = - torch.min(surr1, surr2).mean()
+            actor_loss = - torch.min(surr1, surr2).mean()
             critic_loss = (return_ - value).pow(2).mean()
 
             loss = CRITIC_DISCOUNT * critic_loss + actor_loss - ENTROPY_BETA * entropy
@@ -149,9 +164,9 @@ def ppo_update(frame_idx, states, actions, log_probs, returns, advantages, clip_
             sum_loss_critic += critic_loss
             sum_loss_total += loss
             sum_entropy += entropy
-            
+
             count_steps += 1
-    
+
     writer.add_scalar("returns", sum_returns / count_steps, frame_idx)
     writer.add_scalar("advantage", sum_advantage / count_steps, frame_idx)
     writer.add_scalar("loss_actor", sum_loss_actor / count_steps, frame_idx)
@@ -167,26 +182,27 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # Setup all constant
     setup(args.env)
-    
+
     writer = SummaryWriter(comment="ppo_" + ENV_ID)
-    
+
     # Autodetect CUDA
     use_cuda = torch.cuda.is_available()
-    device   = torch.device("cuda" if use_cuda else "cpu")
+    device = torch.device("cuda" if use_cuda else "cpu")
     print('Device:', device)
-    
+
     # Prepare environments
     envs = [make_env(ENV_ID) for i in range(NUM_ENVS)]
     envs = SubprocVecEnv(envs)
     env = gym.make(ENV_ID)
-    num_inputs  = envs.observation_space.shape[0]
-    num_outputs = envs.action_space.shape[0]
+    num_inputs = NUM_INPUTS #envs.observation_space
+    num_outputs = NUM_OUTPUTS #envs.action_space
 
-    model = ActorCritic(num_inputs, num_outputs, HIDDEN_SIZE).to(device)
+    model_class = getattr(models, MODEL_NAME)
+    model = model_class(num_inputs, num_outputs, HIDDEN_SIZE).to(device)
     print(model)
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    frame_idx  = 0
+    frame_idx = 0
     train_epoch = 0
     best_reward = None
 
@@ -196,11 +212,11 @@ if __name__ == "__main__":
     while not early_stop:
 
         log_probs = []
-        values    = []
-        states    = []
-        actions   = []
-        rewards   = []
-        masks     = []
+        values = []
+        states = []
+        actions = []
+        rewards = []
+        masks = []
 
         for _ in range(PPO_STEPS):
             state = torch.FloatTensor(state).to(device)
@@ -210,43 +226,48 @@ if __name__ == "__main__":
             # each state, reward, done is a list of results from each parallel environment
             next_state, reward, done, _ = envs.step(action.cpu().numpy())
             log_prob = dist.log_prob(action)
-            
+
             log_probs.append(log_prob)
             values.append(value)
             rewards.append(torch.FloatTensor(reward).unsqueeze(1).to(device))
             masks.append(torch.FloatTensor(1 - done).unsqueeze(1).to(device))
-            
+
             states.append(state)
             actions.append(action)
-            
+
             state = next_state
             frame_idx += 1
-                
+
         next_state = torch.FloatTensor(next_state).to(device)
         _, next_value = model(next_state)
-        returns = compute_gae(next_value, rewards, masks, values)
+        returns = compute_gae(next_value, rewards, masks,
+                              values, GAMMA, GAE_LAMBDA)
 
-        returns   = torch.cat(returns).detach()
+        returns = torch.cat(returns).detach()
         log_probs = torch.cat(log_probs).detach()
-        values    = torch.cat(values).detach()
-        states    = torch.cat(states)
-        actions   = torch.cat(actions)
+        values = torch.cat(values).detach()
+        states = torch.cat(states)
+        actions = torch.cat(actions)
         advantage = returns - values
         advantage = normalize(advantage)
-        
-        ppo_update(frame_idx, states, actions, log_probs, returns, advantage)
+
+        ppo_update(frame_idx, states, actions, log_probs, returns, advantage, PPO_EPSILON)
         train_epoch += 1
 
         if train_epoch % TEST_EPOCHS == 0:
-            test_reward = np.mean([test_env(env, model, device) for _ in range(NUM_TESTS)])
+            test_reward = np.mean([test_env(env, model, device)
+                                   for _ in range(NUM_TESTS)])
             writer.add_scalar("test_rewards", test_reward, frame_idx)
             print('Frame %s. reward: %s' % (frame_idx, test_reward))
             # Save a checkpoint every time we achieve a best reward
             if best_reward is None or best_reward < test_reward:
                 if best_reward is not None:
-                    print("Best reward updated: %.3f -> %.3f" % (best_reward, test_reward))
-                    name = "%s_best_%+.3f_%d.dat" % (ENV_ID, test_reward, frame_idx)
+                    print("Best reward updated: %.3f -> %.3f" %
+                          (best_reward, test_reward))
+                    name = "%s_best_%+.3f_%d.dat" % (ENV_ID,
+                                                     test_reward, frame_idx)
                     fname = os.path.join('.', 'checkpoints', name)
                     torch.save(model.state_dict(), fname)
                 best_reward = test_reward
-            if test_reward > TARGET_REWARD: early_stop = True
+            if test_reward > TARGET_REWARD:
+                early_stop = True
